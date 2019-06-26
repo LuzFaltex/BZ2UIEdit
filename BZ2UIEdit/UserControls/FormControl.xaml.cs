357c65c10bf81dd99f299fcd933e7ef8bf284cd8
foxtrek64 <foxtrek64@users.noreply.github.com>
2019-06-26 11:04:48 -0500

    Wire up some menu items

---
+++ BZ2UIEdit/UserControls/FormControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BZ2UIEdit.UserControls
+{
+    /// <summary>
+    /// Interaction logic for FormControl.xaml
+    /// </summary>
+    public partial class FormControl : UserControl
+    {
+        public static readonly DependencyProperty HeaderDependencyProperty
+            = DependencyProperty.Register("Header", typeof(string), typeof(FormControl));
+
+        public string Header
+        {
+            get { return (string) GetValue(HeaderDependencyProperty); }
+            set { SetValue(HeaderDependencyProperty, value); }
+        }
+
+        public static readonly DependencyProperty ToolTipDependencyProperty
+            = DependencyProperty.Register("Hint", typeof(ToolTip), typeof(FormControl));
+
+        public ToolTip Hint
+        {
+            get { return (ToolTip)GetValue(ToolTipDependencyProperty); }
+            set { SetValue(ToolTipDependencyProperty, value); }
+        }
+
+        public FormControl()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
--- BZ2UIEdit/ViewModels/MainPageViewModel.cs
+++ BZ2UIEdit/ViewModels/MainPageViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using BZ2UIEdit.Commands;
+using BZ2UIEdit.Windows;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -19,5 +21,16 @@ namespace BZ2UIEdit.ViewModels
                     tab.IsSelected = true;
                 }                
             });
+
+        public ICommand NewProjectCommand { get; }
+            = new RelayCommand(_ =>
+            {
+                new NewProjectDialog().ShowDialog();
+            });
+
+        public ICommand CloseCommand { get; }
+            = new RelayCommand(
+                _ => Application.Current.MainWindow.Close(),
+                _ => (Application.Current != null && Application.Current.MainWindow != null));
     }
 }
